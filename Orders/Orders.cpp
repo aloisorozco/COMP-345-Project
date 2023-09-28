d3ce7a9a7b2e8df3c4ec396e0cf10dad5097b236
StanOfOld <120013223+StanOfOld@users.noreply.github.com>
2023-09-28 11:55:22 -0400

    Add files via upload
    Did part 3 which should run fine.

---
+++ Orders/Orders.cpp
@@ -0,0 +1,166 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Orders.h"
+
+using namespace std;
+
+bool Order::validate(){
+    //To be implemented
+    return true;
+}
+
+bool Deploy::validate(){
+    //To be implemented
+    return true;
+}
+
+bool Advance::validate(){
+    //To be implemented
+    return true;
+}
+
+bool Bomb::validate(){
+    //To be implemented
+    return true;
+}
+
+bool Blockade::validate(){
+    //To be implemented
+    return true;
+}
+
+bool Airlift::validate(){
+    //To be implemented
+    return true;
+}
+
+bool Negotiate::validate(){
+    //To be implemented
+    return true;
+}
+
+int Order::execute(){
+
+    if(validate()){
+        cout << description + "\n";
+        return 0;
+    }
+
+    return -1;
+}
+
+int Deploy::execute(){
+
+    if(validate()){
+        cout << description + "\n";
+        return 0;
+    }
+
+    return -1;
+}
+
+int Advance::execute(){
+    
+    if(validate()){
+        cout << description + "\n";
+        return 0;
+    }
+
+    return -1;
+}
+
+int Bomb::execute(){
+    
+    if(validate()){
+        cout << description + "\n";
+        return 0;
+    }
+
+    return -1;
+}
+
+int Blockade::execute(){
+    
+    if(validate()){
+        cout << description + "\n";
+        return 0;
+    }
+
+    return -1;
+}
+
+int Airlift::execute(){
+    
+    if(validate()){
+        cout << description + "\n";
+        return 0;
+    }
+
+    return -1;
+}
+
+int Negotiate::execute(){
+    
+    if(validate()){
+        cout << description + "\n";
+        return 0;
+    }
+
+    return -1;
+}
+
+int OrdersList::add(Order order){
+
+    orders.push_back(order);
+    return 0;
+}
+
+int OrdersList::remove(int index){
+
+    if (index >= 0 && index < orders.size()) {
+
+        orders.erase(orders.begin() + index);
+        return 0;
+    }
+
+    else{
+
+        cout << "Invalid index: " << (index) << " \n";
+        return -1;
+    }
+}
+
+int OrdersList::remove(){
+
+    return remove(orders.size() - 1);
+}
+
+int OrdersList::move(int index1, int index2){
+
+    if (index1 >= 0 && index1 < orders.size() && index2 >= 0 && index2 < orders.size() && index1 != index2) {
+
+        Order ordermove = orders[index1];
+        orders.erase(orders.begin() + index1);
+
+        orders.insert(orders.begin() + index2, ordermove);
+        return 0;
+    }
+
+    else{
+        cout << "Invalid index(es)" << index1 << ", " << index2 << "\n";
+        return -1;
+    }
+}
+
+int OrdersList::executeAll(){
+
+    for(Order order : orders){
+
+        if(order.execute() != 0){
+            return -1;
+        }
+    }
+
+    return 0;
+}
--- Orders/Orders.h
+++ Orders/Orders.h
@@ -1,62 +1,115 @@
-#ifndef ORDERS_H
-#define ORDERS_H
+#ifndef Orders
+#define Orders
 
-class OrderList {
-
-public:
-	OrderList();
-
-	addOrder(Order* order);
-
-};
+#include <iostream>
+#include <string>
+#include <vector>
 
 class Order {
 
-public:
-	Order();
+    protected:
+        std::string description;
+
+    public:
+        Order() : description("This is a generic order."){
+
+        }
+
+        virtual bool validate();
+        virtual int execute();
+
+        friend std::ostream& operator<<(std::ostream& os, const Order& order) {
+            os << order.getDescription();
+            return os;
+        }
+
+        virtual std::string getDescription() const {
+            return "This is a generic order description.";
+        }
+};
+
+class Deploy : public Order{
+
+    public:
+        Deploy(){
+            description = "This is a deploy order";
+        }
+
+        bool validate() override;
+        int execute() override;
 
 };
 
-class Deploy : public Order {
+class Advance : public Order{
 
-public:
-	Deploy();
+    public:
+        Advance(){
+            description = "This is an Advance order";
+        }
 
-};
-
-class Advance : public Order {
-
-public:
-	Advance();
+        bool validate() override;
+        int execute() override;
 
 };
 
-class Bomb : public Order {
+class Bomb : public Order{
 
-public:
-	Bomb();
+    public:
+        Bomb(){
+            description = "This is a Bomb order";
+        }
 
-};
-
-class Blockade : public Order {
-
-public:
-	Blockade();
+        bool validate() override;
+        int execute() override;
 
 };
 
-class Airlift : public Order {
+class Blockade : public Order{
 
-public:
-	Airlift();
+    public:
+        Blockade(){
+            description = "This is a Blockade order";
+        }
+
+        bool validate() override;
+        int execute() override;
+};
+
+class Airlift : public Order{
+
+    public:
+        Airlift(){
+            description = "This is an Airlift order";
+        }
+
+        bool validate() override;
+        int execute() override;
 
 };
 
-class Negotiate : public Order {
+class Negotiate : public Order{
 
-public:
-	Negotiate();
+    public:
+        Negotiate(){
+            description = "This is a Negotiate order";
+        }
 
+        bool validate() override;
+        int execute() override;
+
+};
+
+class OrdersList {
+
+    private:
+        std::vector<Order> orders;
+
+    public:
+        int add(Order order);
+        int move(int index1, int index2);
+        int remove();
+        int remove(int index);
+        int executeAll();
 };
 
 #endif
