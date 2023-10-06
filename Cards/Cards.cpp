d9a64279a22b4b057e7214132a5706fa82c225d0
Maximilian <70289545+MaxGrabs@users.noreply.github.com>
2023-10-05 20:11:00 -0400

    commit_2
    1.all data members of class type are pointers
    4.added these requirements too
    5. should be done
    6. alot of comments

--- Cards/Cards.cpp
+++ Cards/Cards.cpp
@@ -1,16 +1,27 @@
 #include "Cards.h"
+//#include "../Orders/Orders.h"
 #include <iostream>
 #include <string>
 #include <algorithm>
-#include "../Orders/Orders.h"
-
 using namespace std;
-
+//////////CARD////////////////
 Card::Card(CardType type) : type(type) {}
-CardType Card::getType()const{
-    return type;
+//Documentation on copy constructor follows this format
+Card::Card(const Card& other) : type(other.type) {}
+//assignment operator
+Card &Card::operator=(const Card &) {
+    return *this;
+}
+//stream insertion
+ostream &operator<<(ostream& out, const Card& card) {
+    out <<"card type = " << card.cardTypeToString() << endl;
+    return out;
+}
+Card::CardType Card::getType() const{
+    return type;//returns numbers 0-4(5 types of cards)
 }
 string Card::cardTypeToString() const {
+    //check type, return the string value
     if(type == CardType::Bomb){
         return "Bomb";
     }
@@ -27,60 +38,175 @@ string Card::cardTypeToString() const {
         return "Diplomacy";
     }
     else{
-        return "Error detecting Card Type";
+        return "Error detecting Card Type : " + to_string(static_cast<int>(type));
     }
 }
-void Card::play(Deck& deck){
-    cout <<"Playing a " << cardTypeToString() << " card." << endl;
-    //Add card to Order list
-    deck.returnCard(*this);
+//check hand size, to make sure a card can be played
+void Card::play(Deck **deck, Hand **hand){
+    if((*hand)->getHandSize() == 0){
+        cout <<"No cards to play."<<endl;
+        return;
+    }
+    cout <<"Playing a " << this->cardTypeToString() << " card." << endl;
+    //Order* order; // play() creates an order
+    if(type == CardType::Bomb){
+        //order = new Bomb(); //havent fetched/merged Order
+        cout<<"Creating new Bomb Order to Order List\n";
+        //ordersList->add(*order);
+    }
+    else if(type == CardType::Reinforcement){
+        //order = new Reinforcement();
+        cout<<"Creating new Reinforcement Order to Order List\n";
+        //ordersList->add(*order);
+    }
+    else if(type == CardType::Blockade){
+        //order = new Blockade();
+        cout<<"Creating new Blockade Order to Order List\n";
+        //ordersList->add(*order);
+    }
+    else if(type == CardType::Airlift){
+        //order = new Airlift();
+        cout<<"Creating new Airlift Order to Order List\n";
+        //ordersList->add(*order);
+    }
+    else if(type == CardType::Diplomacy){
+        //order = new Diplomacy();
+        cout<<"Creating new Diplomacy Order to Order List\n";
+        //ordersList->add(*order);
+    }
+    //print the card played
+    (*deck)->returnCard(this);
+    (*hand)->removeCard(this);
 }
+//Deconstructor, but Deck and Hand already clear the cards
+Card::~Card() = default;
+
+/////////////////////DECK//////////////////////////
 Deck::Deck(){
-    initializeDeck();
+    initializeDeck(); // just adds cards to deck
+}
+//copy constructor
+Deck::Deck(const Deck &) {}
+//assignment operator
+Deck &Deck::operator=(const Deck &) {
+    return *this;
+}
+//stream insertion
+ostream &operator<<(ostream& out, const Deck& deck) {
+    out << "\nCards left in deck:" <<endl;
+    for(const Card* card : deck.cards){
+        out <<"Card type: " << card->cardTypeToString()<<endl;
+    }
+    return out;
 }
 //No idea what would trigger Draw method.
 //In the game Risk, you draw one at end of turn if you captured territory.
-Card Deck::draw(){
+void Deck::draw(Hand **hand){
     //first we shuffle, then we draw.
-    //not going to use shuffle() or some costly methods to randomize.
-    if(!cards.empty()) {
-        int randIndex = rand() % cards.size();
-        Card card = cards[randIndex];
-        cards.erase(cards.begin() + randIndex);
-        cout<<"You drew the card: "<< card.cardTypeToString() << " card." << endl;
-        return card;
-    }
+   if(!cards.empty()) {
+        int randIndex = rand() % cards.size(); // rand() generates same numbers, fine for now
+        auto card = cards.begin() + randIndex;
+        Card* drawncard = *card;
+        cout<<"You drew the card: "<< drawncard->cardTypeToString() << " card." << endl;
+       (*hand) -> addCard(drawncard);
+        cards.erase(card);
+   }
     else{
         //In theory this should happen if we draw the whole deck without playing cards.
-        cout<<"EMPTY DECK ";
+        cout<<"EMPTY DECK "<<endl;
         throw runtime_error("EMPTY DECK ");
     }
 }
 //We want to return the Card after playing it.
-void Deck::returnCard(Card& card) {
+void Deck::returnCard(Card* card) {
     cards.push_back(card);
 }
 //Game Start, put cards into Deck.
 void Deck::initializeDeck() {
     //Put cards of all types into deck. NOT SHUFFLED
     //need to know how many cards in the deck(NOT SPECIFIED)
-    int deckSize = 15;
+    // or if it's going to be a user defined amount(input)
+    int temp = 2;
+    setDeckSize(temp);
     //emplace_back appends new element to the end
     for(int i =0; i<deckSize; i++){
-        cards.emplace_back(CardType::Bomb);
-        cards.emplace_back(CardType::Reinforcement);
-        cards.emplace_back(CardType::Blockade);
-        cards.emplace_back(CardType::Airlift);
-        cards.emplace_back(CardType::Diplomacy);
+        cards.emplace_back(new Card(Card::Bomb));
+        cards.emplace_back(new Card(Card::Reinforcement));
+        cards.emplace_back(new Card(Card::Blockade));
+        cards.emplace_back(new Card(Card::Airlift));
+        cards.emplace_back(new Card(Card::Diplomacy));
     }
 }
-Hand::Hand(int handSize){
-    //
-    handSize = handSize;
-};
-void Hand::addCard(Card card) {
-    cards.push_back(card);
+
+Deck::~Deck() {
+    for(auto & card : cards){
+        delete card; // delete the Card pointers in the vector
+    }
+    cards.clear(); // clear the vector
 }
-const vector<Card>& Hand::getCards() const{
+
+int Deck::getDeckSize() const {
+    return cards.size();
+}
+
+void Deck::setDeckSize(int s) {
+    deckSize =s;
+}
+///////////////HAND//////////////////
+
+Hand::Hand(Deck* deck){}
+Hand::Hand(const Hand &) {}
+
+const vector<Card *> & Hand::getCards() const {
+    if(cards.empty()){
+        //prevent getting from empty hand.
+        throw runtime_error("No cards in hand");
+    }
     return cards;
 }
+void Hand::addCard(Card *card) {
+    cards.push_back(card);
+    handSize++;
+}
+
+Hand& Hand::operator=(const Hand& hand) {
+    return *this;
+}
+
+ostream &operator<<(ostream& out, const Hand& hand) {
+
+    out <<"\nNumber of cards in hand: "<<hand.handSize<<"\ncards in hand : "<<endl;
+    for (auto card : hand.cards) {
+        out <<"Card type: "<< card->cardTypeToString()<<endl ;
+    }
+    return out;
+}
+
+Hand::~Hand() {
+    for(Card* card: cards){
+        delete card;
+    }
+    cards.clear();
+}
+
+int Hand::getHandSize() const {
+    return handSize;
+}
+
+void Hand::setHandSize(int s) {
+    handSize =s;
+}
+
+void Hand::removeCard(Card* card) {
+    auto index = find(cards.begin(), cards.end(), card);
+    if(index != cards.end()){
+        cards.erase(index);
+        //card = nullptr;
+        handSize--;
+    }
+
+}
+
+
+
+
--- Cards/Cards.h
+++ Cards/Cards.h
@@ -1,58 +1,76 @@
 #ifndef CARDS_H
 #define CARDS_H
-
+//#include "../Orders/Orders.h"
 #include <vector>
 #include <string>
 using namespace std;
 
-//Forward declarations is good practice
 class Card;
-class Deck;
+class Deck;//forward declaration is good practice
 class Hand;
-//The card Types
-enum class CardType {
-    Bomb,
-    Reinforcement,
-    Blockade,
-    Airlift,
-    Diplomacy
-};
-/*
- * REQUIREMENTS FOR A1
- * draw() -> draw a card from deck, remove from deck
- * returnCard() -> play() needs to return the card to deck
- */
-class Deck {
+
+class Deck { //Deck class contains Cards, players will draw cards with draw() and return cards returnCard()
 public:
-	Deck();
-    Card draw();
-    void returnCard(Card& card);
+    Deck(); //default constructor
+	Deck(const Deck&); //1. our copy constrctor
+    Deck& operator =(const Deck&); //2.copy operator
+    friend ostream& operator<<(ostream&,const Deck&);//3.stream insertion operator for output
+
+    void draw(Hand **hand); // draws a card, removes it from deck adds it to hand
+    void returnCard(Card* card); // played card is removed from hand, returned to deck
+    //potential usage of getter/setter
+    int getDeckSize()const;
+    void setDeckSize(int s);
+    ~Deck(); // deconstructor
 private:
-    vector<Card> cards;
-    void initializeDeck();
+    vector<Card*> cards; //vector data type is best.Card* , user defined class data type is pointer
+    int deckSize;
+    void initializeDeck(); // quick method to add all cards to deck.
 };
-/*
- * Draw card -> addCard to hand.
- */
-class Hand {
+
+class Hand {//Hand class contains card drawn from Deck. Unique to each player.
 public:
-    explicit Hand(int handSize);
-    int handSize;
-	void addCard(Card card);
-    const vector<Card>& getCards() const;
+    explicit Hand(Deck*); // default constructor, taking card from the Deck.
+    Hand(const Hand&); // 1.our copy constructor
+    Hand& operator =(const Hand& hand);// 2. our copy operator
+    friend ostream& operator<<(ostream&,const Hand&);//3.stream insertion operator for output
+
+    void addCard(Card *card); // no need to modify so const
+    void removeCard(Card *card);
+    const vector<Card *> & getCards() const; //
+    //Setter getter handsize might be useful later
+    void setHandSize(int s);
+    int getHandSize() const;
+    ~Hand(); // deconstructor
+
 private:
-    vector<Card> cards;
+    //Temporary for adding orders on play()
+    //OrdersList* ordersList;
+    //the current hand size
+    int handSize = 0;
+    //maybe a max hand size later on??
+    vector<Card*> cards; //we want pointers to cards
+    //we want to delete these after in deconstructor
 };
-/*
- * REQUIREMENTS FOR A1
- * play() -> play a card.
- */
-class Card{
+
+class Card{//Card class, different types of cards, play()
 public:
-    explicit Card(CardType type);
-    void play(Deck& deck);
+    enum CardType {
+        Bomb,
+        Reinforcement,
+        Blockade,
+        Airlift,
+        Diplomacy
+    };
+    Card(CardType type); //default constructor
+    Card(const Card&); //copy constructor, const for no altering
+    Card& operator =(const Card&); //Assignment operator of value of Card
+    friend ostream& operator<<(ostream&, const Card&);//stream insertion operator for output
+
+    void play(Deck **deck, Hand **hand); // The play function we need
     CardType getType() const;
     string cardTypeToString() const;
+    ~Card(); //Deconstructor for destroying the card if ever needed later
 private:
     CardType type;
 };
