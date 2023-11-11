ae9e14819c751c48cefd93b8a98ac71a1b7fc97b
SamPal <trashsn0779@gmail.com>
2023-11-10 22:08:29 -0500

    made readCommand() private. getCommand() -> public

--- CommandProcessor/CommandProcessingDriver.cpp
+++ CommandProcessor/CommandProcessingDriver.cpp
@@ -2,13 +2,13 @@
 #include "CommandProcessing.h"
 #include <iostream>
 
-
-
 void testCommandProcessor() {
     int choice;
     std::string filename;
+
     // Initialisation of the engine
     GameEngine engine;
+
 
     std::cout << "Pick between the two options:" << std::endl;
     std::cout << "Console Commands (1)" << std::endl;
@@ -16,25 +16,25 @@ void testCommandProcessor() {
     std::cout << "> ";
     std::cin >> choice;
 
-    if (choice == 1) {
-        ConsoleCommandProcessor consoleProcessor(&engine);
-        consoleProcessor.readCommand();
+    
 
-        while (!consoleProcessor.isCommandsEmpty()) {
-            Command cmd = consoleProcessor.getCommand();
-            std::cout << "Command from console: " << cmd.command << ", Effect: " << cmd.effect << std::endl;
-        }
+    if (choice == 1) {
+        // Initialisation of the CommandProcessor
+        ConsoleCommandProcessor consoleProcessor(&engine);
+
+        consoleProcessor.getCommand();
+
+        
     } else if (choice == 2) {
+        
         std::cout << "Enter filename: ";
         std::cin >> filename;
 
-        FileCommandProcessorAdapter fileProcessor(filename, &engine);
-        fileProcessor.readCommand();
+        // Initialisation of the CommandProcessor
+        FileCommandProcessorAdapter fileProcessor(filename,&engine);
 
-        while (!fileProcessor.isCommandsEmpty()) {
-            Command cmd = fileProcessor.getCommand();
-            std::cout << "Command from file: " << cmd.command << ", Effect: " << cmd.effect << std::endl;
-        }
+        fileProcessor.getCommand();
+        
     } else {
         std::cout << "Invalid choice. Please enter 1 or 2." << std::endl;
     }
